d5551ef597696fc2210db30eeb8f96d40e3fbe54
gardenerc <61226395+gardenerc@users.noreply.github.com>
2023-04-07 00:18:19 -0700

    Update news_word_of_the_day.py

--- news_word_of_the_day.py
+++ news_word_of_the_day.py
@@ -1,76 +1,57 @@
 import requests
-from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
-import io
-import time
-import threading
+from collections import Counter
+import re
+import tkinter as tk
+from tkinter import font as tkfont
 
-# Function to fetch news headlines
-def fetch_news():
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=8856e4b4e7b74fa091fa9bbd2c194766"
+def fetch_headlines(api_key):
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
     response = requests.get(url)
     data = response.json()
-    headlines = [article["title"] for article in data["articles"]]
+    headlines = [article['title'] for article in data['articles']]
     return headlines
 
-# Function to extract the word of the day and its context
-def word_of_the_day(headlines):
-    # Add your code to process the headlines, find the word of the day and its context
-    # This is just a placeholder example. Replace it with your actual implementation.
-    word = "example"
-    context = "This is an example context for the word of the day."
-    return word, context
+def find_interesting_words(headlines):
+    common_words = {'the', 'and', 'in', 'a', 'to', 'of', 'on', 'for', 'with'}
+    words = [word.lower() for headline in headlines for word in re.findall(r'\b\w+\b', headline) if word.lower() not in common_words]
+    word_counts = Counter(words)
+    interesting_words = word_counts.most_common(4)
+    return interesting_words
 
-# Function to update the label with the word of the day and its context
-def update_label():
-    headlines = fetch_news()
-    word, context = word_of_the_day(headlines)
-    word_label.config(text=f"Word of the day: {word}")
-    context_label.config(text=f"Context: {context}")
+def find_example_headlines(interesting_words, headlines):
+    example_headlines = []
+    for word, _ in interesting_words:
+        for headline in headlines:
+            if word in headline.lower():
+                example_headlines.append((word, headline))
+                break
+    return example_headlines
 
-    # Schedule the function to run again after 60 seconds
-    root.after(60000, update_label)
+def create_gui(example_headlines):
+    root = tk.Tk()
+    root.title("News Word of the Day")
+    root.configure(bg='#1DA1F2')
 
-# Function to load and display the animated GIF
-def update_gif(ind):
-    frame = frames[ind]
-    ind += 1
-    if ind == frame_count:
-        ind = 0
-    gif_label.config(image=frame)
-    root.after(100, update_gif, ind)
+    bold_font = tkfont.Font(weight='bold')
 
-root = Tk()
-root.title("News Word of the Day")
-root.geometry("400x400")
+    description_label = tk.Label(root, text="The 4 most frequent interesting words in today's news headlines:", bg='#1DA1F2', font=bold_font)
+    description_label.pack(pady=10)
 
-canvas = Canvas(root, width=400, height=400)
-canvas.pack(fill=BOTH, expand=True)
+    for word, headline in example_headlines:
+        word_label = tk.Label(root, text=f"{word}:", bg='#1DA1F2', font=bold_font)
+        word_label.pack(anchor='w')
 
-gradient = ttk.Style()
-gradient.configure("Grad.TFrame", background="#ADD8E6")
+        example_label = tk.Label(root, text=headline, bg='#1DA1F2', wraplength=600, justify='left')
+        example_label.pack(anchor='w', padx=20)
 
-frame = ttk.Frame(root, style="Grad.TFrame")
-frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+    root.mainloop()
 
-word_label = ttk.Label(frame, text="")
-word_label.pack(pady=10)
-context_label = ttk.Label(frame, text="")
-context_label.pack(pady=10)
+def main():
+    api_key = '8856e4b4e7b74fa091fa9bbd2c194766'
+    headlines = fetch_headlines(api_key)
+    interesting_words = find_interesting_words(headlines)
+    example_headlines = find_example_headlines(interesting_words, headlines)
+    create_gui(example_headlines)
 
-# Load and display the animated GIF
-url = "https://j.gifs.com/KYo5qo.gif"
-response = requests.get(url)
-gif_data = response.content
-gif = Image.open(io.BytesIO(gif_data))
-frames = [ImageTk.PhotoImage(gif.copy()) for i in range(gif.n_frames)]
-frame_count = len(frames)
-
-gif_label = ttk.Label(frame)
-gif_label.pack(pady=10)
-
-update_label()
-update_gif(0)
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
